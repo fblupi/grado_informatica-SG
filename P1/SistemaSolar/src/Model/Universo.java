cd8c82a21c7f99b9e519d6f3291d02a27cef5742
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-04-07 14:46:05 +0200

    mejorada cámara luna

--- P1/SistemaSolar/src/Model/Universo.java
+++ P1/SistemaSolar/src/Model/Universo.java
@@ -30,7 +30,7 @@ public class Universo {
         camaraPlanta = new Camara(false, false, canvas, new Point3d (0,200,0), new Point3d (0,0,0), new Vector3d (0,0,-1), 0.0032f, 0.3f, 100.0f);
         camaraPerspectiva = new Camara(true, true, canvasVariable, new Point3d (80,80,80), new Point3d (0,0,0), new Vector3d (0,1,0), 45.0f, 0.3f, 100.0f);
         camaraNave = new Camara(true, false, canvasVariable, new Point3d (0,0.5,-0.25), new Point3d (0,0,1), new Vector3d (0,1,0), 45.0f, 0.1f, 30.0f);
-        camaraLuna = new Camara(true, false, canvasVariable, new Point3d (0,0.45,0), new Point3d (-1,-0.25,0), new Vector3d (1,1,0), 90.0f, 0.05f, 15.0f);
+        camaraLuna = new Camara(true, false, canvasVariable, new Point3d (0,0.5,0), new Point3d (-1,-0.25,0), new Vector3d (1,1,0), 100.0f, 0.05f, 15.0f);
         
         locale.addBranchGraph(camaraPerspectiva);
         locale.addBranchGraph(camaraPlanta);
